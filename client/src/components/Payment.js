cb54c834dbb30d355781e50a6ad497f84d487bfe
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:39:07 +0800

    Create Payment.js

---
+++ client/src/components/Payment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Payment() {
+    return (
+        <div>
+            <h2>Payment Page</h2>
+            {/* Payment logic goes here */}
+        </div>
+    );
+}
+
+export default Payment;

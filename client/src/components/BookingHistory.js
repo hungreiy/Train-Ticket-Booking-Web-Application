65a75e51b8d71dbad8addc3de4f9a2aff7a503a0
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:38:07 +0800

    Create BookingHistory.js

---
+++ client/src/components/BookingHistory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BookingHistory() {
+    return (
+        <div>
+            <h2>Booking History</h2>
+            {/* Booking history logic goes here */}
+        </div>
+    );
+}
+
+export default BookingHistory;

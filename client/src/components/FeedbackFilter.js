ea98454dcb3a6dbeec195cec5a8200b780613a35
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:41:59 +0800

    Create FeedbackFilter.js

---
+++ client/src/components/FeedbackFilter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function FeedbackFilter({ onFilter }) {
+    return (
+        <div>
+            <h3>Filter Feedback</h3>
+            {/* Filter logic goes here */}
+        </div>
+    );
+}
+
+ ```javascript
+export default FeedbackFilter;

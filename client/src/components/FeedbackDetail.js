1b674a8cf0c657877ac8c2b597711cc9f125777a
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:43:18 +0800

    Create FeedbackDetail.js

---
+++ client/src/components/FeedbackDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FeedbackDetail() {
+    return (
+        <div>
+            <h2>Feedback Detail</h2>
+            {/* Feedback detail logic goes here */}
+        </div>
+    );
+}
+
+export default FeedbackDetail;

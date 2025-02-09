4b090bb4411fe7d9757edd0f21842ebfb2d64beb
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:36:45 +0800

    Create Register.js

---
+++ client/src/components/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Register() {
+    return (
+        <div>
+            <h2>Register Page</h2>
+            {/* Registration logic goes here */}
+        </div>
+    );
+}
+
+export default Register;

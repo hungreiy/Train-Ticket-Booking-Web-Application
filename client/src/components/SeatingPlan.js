c1f788eb6e58b444ae5344cf81484df48d793638
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:37:37 +0800

    Create SeatingPlan.js

---
+++ client/src/components/SeatingPlan.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SeatingPlan() {
+    return (
+        <div>
+            <h2>Seating Plan</h2>
+            {/* Seating plan logic goes here */}
+        </div>
+    );
+}
+
+export default SeatingPlan;

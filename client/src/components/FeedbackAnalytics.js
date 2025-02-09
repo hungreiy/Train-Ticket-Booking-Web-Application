bd1fedac7b14381b7fb1d17ed1a7feeb01394ade
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:43:42 +0800

    Create FeedbackAnalytics.js

---
+++ client/src/components/FeedbackAnalytics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FeedbackAnalytics() {
+    return (
+        <div>
+            <h2>Feedback Analytics</h2>
+            {/* Analytics logic goes here */}
+        </div>
+    );
+}
+
+export default FeedbackAnalytics;

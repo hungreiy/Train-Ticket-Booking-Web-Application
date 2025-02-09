74a20fd48613f6e362bd35a2374567e7db713e66
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:41:35 +0800

    Create TrainDetails.js

---
+++ client/src/components/TrainDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TrainDetails() {
+    return (
+        <div>
+            <h2>Train Details</h2>
+            {/* Train details logic goes here */}
+        </div>
+    );
+}
+
+export default TrainDetails;

f7d304bbb6a6851fa0e5732e52700d91c5afeb5e
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:36:15 +0800

    Create Login.js

---
+++ client/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <div>
+            <h2>Login Page</h2>
+            {/* Login logic goes here */}
+        </div>
+    );
+}
+
+export default Login;

9310301b70e39255f0d98f1b30d89bebf44c6352
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:42:52 +0800

    Create FeedbackComment.js

---
+++ client/src/components/FeedbackComment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FeedbackComment() {
+    return (
+        <div>
+            <h2>Feedback Comment</h2>
+            {/* Comment logic goes here */}
+        </div>
+    );
+}
+
+export default FeedbackComment;

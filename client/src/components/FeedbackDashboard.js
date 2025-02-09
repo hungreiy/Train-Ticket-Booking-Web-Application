158d2abbc44b8b915353805015ceab7355841bbb
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:42:30 +0800

    Create FeedbackDashboard.js

---
+++ client/src/components/FeedbackDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FeedbackDashboard() {
+    return (
+        <div>
+            <h2>Feedback Dashboard</h2>
+            {/* Dashboard logic goes here */}
+        </div>
+    );
+}
+
+export default FeedbackDashboard;

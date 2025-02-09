5e61592fe220151f904d970f75cff67dbd589c3a
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:44:06 +0800

    Create NotificationSettings.js

---
+++ client/src/components/NotificationSettings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotificationSettings() {
+    return (
+        <div>
+            <h2>Notification Settings</h2>
+            {/* Notification settings logic goes here */}
+        </div>
+    );
+}
+
+export default NotificationSettings;

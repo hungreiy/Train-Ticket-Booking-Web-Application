4dfec1ea10ba3b71bf37705cc984059d0d9e76c8
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:38:42 +0800

    Create Profile.js

---
+++ client/src/components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Profile() {
+    return (
+        <div>
+            <h2>User Profile</h2>
+            {/* Profile logic goes here */}
+        </div>
+    );
+}
+
+export default Profile;

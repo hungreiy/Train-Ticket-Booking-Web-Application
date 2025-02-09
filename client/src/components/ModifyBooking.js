2cd4048bfde8d07fbd8a0bd3e748d4e577e282f2
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:40:27 +0800

    Create ModifyBooking.js

---
+++ client/src/components/ModifyBooking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ModifyBooking() {
+    return (
+        <div>
+            <h2>Modify Booking</h2>
+            {/* Modify booking logic goes here */}
+        </div>
+    );
+}
+
+export default ModifyBooking;

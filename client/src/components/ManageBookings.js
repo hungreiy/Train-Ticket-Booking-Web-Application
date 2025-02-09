e67702b79132553107a66a5add0b3859217b15e9
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:40:02 +0800

    Create ManageBookings.js

---
+++ client/src/components/ManageBookings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ManageBookings() {
+    return (
+        <div>
+            <h2>Manage Bookings</h2>
+            {/* Manage bookings logic goes here */}
+        </div>
+    );
+}
+
+export default ManageBookings;

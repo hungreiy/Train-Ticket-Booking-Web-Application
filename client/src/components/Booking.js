ebacae0cdb8ce6723b048c52ecf6deb5f385974e
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:35:43 +0800

    Create Booking.js

---
+++ client/src/components/Booking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Booking() {
+    return (
+        <div>
+            <h2>Booking Page</h2>
+            {/* Booking logic goes here */}
+        </div>
+    );
+}
+
+export default Booking;

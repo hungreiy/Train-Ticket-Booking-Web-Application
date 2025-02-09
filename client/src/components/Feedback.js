0e83f4528aeac5b0558b1b654ca9893299c1086a
hungreiy <130607832+hungreiy@users.noreply.github.com>
2025-02-09 18:40:57 +0800

    Create Feedback.js

---
+++ client/src/components/Feedback.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Feedback() {
+    return (
+        <div>
+            <h2>Feedback Page</h2>
+            {/* Feedback logic goes here */}
+        </div>
+    );
+}
+
+export default Feedback;
